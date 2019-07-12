6572f060602e6da301be1a9c70e5310bbc39dfa0
johnny <azxazx541@gmail.com>
2019-07-12 12:06:30 +0800

    second

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class Builder extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, mode) {
         if (mode.maxWidth > 500) return DekstopPage();
-
         return MobilePage();
       },
     );
@@ -32,48 +31,7 @@ class MobilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Image.network(
-                    "http://skolko-poluchaet.ru/wp-content/uploads/2018/02/skolko-zarabatyvayut-kosmonavty-768x432.jpg"),
-                Text("Антон Чертовских"),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text("Обо мне"),
-                      Text("Работы"),
-                      Text("Со мной нельзя связаться")
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 7,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Ничего не умею"),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+        children: <Widget>[new Avatar(), new Menu()],
       ),
     );
   }
@@ -84,47 +42,80 @@ class DekstopPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
+        children: <Widget>[new Avatar(), new Menu()],
+      ),
+    );
+  }
+}
+
+
+class Avatar extends StatelessWidget {
+  const Avatar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 200,
+            height: 200,
+            child: Image.network(
+              "http://skolko-poluchaet.ru/wp-content/uploads/2018/02/skolko-zarabatyvayut-kosmonavty-768x432.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 30),
+            child: Text(
+              "Антон Чертовских",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class Menu extends StatelessWidget {
+  const Menu({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Expanded(
             flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Image.network(
-                    "http://skolko-poluchaet.ru/wp-content/uploads/2018/02/skolko-zarabatyvayut-kosmonavty-768x432.jpg"),
-                Text("Антон Чертовских"),
+                Text("Обо мне"),
+                Text("Работы"),
+                Text("Связаться")
               ],
             ),
           ),
           Expanded(
-            flex: 1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+            flex: 7,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text("Обо мне"),
-                      Text("Работы"),
-                      Text("Со мной нельзя связаться")
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 7,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Ничего не умею"),
-                    ],
-                  ),
-                ),
+                Text("Ничего не умею"),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
